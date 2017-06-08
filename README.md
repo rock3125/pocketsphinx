979a4f727b882f48cc07384e0944d4d4b52f22a8
Rock de Vocht <peter@peter.co.nz>
2017-06-08 15:46:50 +1200

    added better file based recognition and time based output

--- README.md
+++ README.md
@@ -2,11 +2,19 @@
 
 First install the required apt dependencies
 ```
-sudo apt install pocketsphinx
-sudo apt install swig
-sudo apt install libpulse-dev
+sudo apt install pocketsphinx swig libpulse-dev
 ```
 and then continue with the pocketsphinx python wrapper
 ```
 pip install --upgrade pocketsphinx
 ```
+
+Download latest LM and HMM from
+```
+https://sourceforge.net/projects/cmusphinx/files/Acoustic%20and%20Language%20Models/US%20English/
+```
+The HMM is the `cmusphinx-en-us-5.2.tar.gz` file (which needs to be extracted to use)
+The LM is the `en-70k-0.2.lm.gz` file.
+
+Converting a text apra model to binary
+`sphinx_lm_convert -i model.lm -o model.lm.bin`
--- from_file.py
+++ from_file.py
@@ -1,23 +1,40 @@
 #!/usr/bin/env python
 
 import os
-from pocketsphinx import AudioFile, get_model_path, get_data_path
 
-model_path = get_model_path()
-data_path = get_data_path()
+from pocketsphinx.pocketsphinx import *
+from pocketsphinx import get_model_path
 
-config = {
-    'verbose': False,
-    'audio_file': 'r_nixon_short.wav',
-    'buffer_size': 2048,
-    'no_search': False,
-    'full_utt': False,
-    'hmm': os.path.join(model_path, 'en-us'),
-    'lm': os.path.join(model_path, 'en-us.lm.bin'),
-    'dict': os.path.join(model_path, 'cmudict-en-us.dict')
-}
+base_dir = os.path.dirname(__file__)
 
-audio = AudioFile(**config)
-for phrase in audio:
-    print(phrase)
+# Create a decoder with certain model
+config = Decoder.default_config()
+config.set_string('-hmm', os.path.join(base_dir,'data/cmusphinx-en-us-5.2'))
+config.set_string('-lm', os.path.join(base_dir, 'data/en-70k-0.2.lm.bin'))
+config.set_string('-dict', os.path.join(get_model_path(), 'cmudict-en-us.dict'))
+# switch off noisy logging (very useful for debugging when it's not working)
+config.set_string('-logfn', '/dev/null')
 
+# config.set_string('-inmic', 'yes')
+
+# Decode streaming data.
+decoder = Decoder(config)
+
+# input file must be 16KHz mono for this to work
+decoder.start_utt()
+stream = open(os.path.join(base_dir, 'data/r_nixon_short.wav'), 'rb')
+while True:
+    buf = stream.read(1024)
+    if buf:
+        decoder.process_raw(buf, False, False)
+    else:
+        break
+decoder.end_utt()
+
+hypothesis = decoder.hyp()
+# logmath = decoder.get_logmath()
+print('Best hypothesis: ' + hypothesis.hypstr) # , " model score: ", hypothesis.best_score, " confidence: ", logmath.exp(hypothesis.prob))
+
+# output segments
+for seg in decoder.seg():
+    print(seg.word + " [" + str(seg.start_frame) + "-" + str(seg.end_frame) + "]")
