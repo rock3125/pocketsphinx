d0860de6f7ec9eb8917930ad01d50cd45d0d9f40
Rock de Vocht <peter@peter.co.nz>
2017-06-08 16:23:56 +1200

    made modifications to listening and talking

--- continuous.py
+++ continuous.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python
 
-from pocketsphinx import LiveSpeech
+import os
+from pocketsphinx import LiveSpeech, get_model_path
 
-for phrase in LiveSpeech():
-    print(phrase)
+base_dir = os.path.dirname(__file__)
 
+if __name__ == '__main__':
+
+    speech = LiveSpeech(
+        verbose=False,
+        sampling_rate=16000,
+        buffer_size=2048,
+        no_search=False,
+        full_utt=False,
+        hmm=os.path.join(base_dir, os.path.join(base_dir,'data/cmusphinx-en-us-5.2')),
+        lm=os.path.join(base_dir, 'data/en-70k-0.2.lm.bin'),
+        dic=os.path.join(get_model_path(), 'cmudict-en-us.dict')
+    )
+
+    print("start talking, I'm listening.  Say \"stop\" to stop this program.")
+    for phrase in LiveSpeech():
+        phrase_str = str(phrase)
+        print(phrase_str)
+        if phrase_str == "stop" or phrase_str == "stop it":
+            break
--- from_file.py
+++ from_file.py
@@ -1,40 +1,56 @@
 #!/usr/bin/env python
 
 import os
+import sys
 
 from pocketsphinx.pocketsphinx import *
 from pocketsphinx import get_model_path
 
 base_dir = os.path.dirname(__file__)
 
-# Create a decoder with certain model
-config = Decoder.default_config()
-config.set_string('-hmm', os.path.join(base_dir,'data/cmusphinx-en-us-5.2'))
-config.set_string('-lm', os.path.join(base_dir, 'data/en-70k-0.2.lm.bin'))
-config.set_string('-dict', os.path.join(get_model_path(), 'cmudict-en-us.dict'))
-# switch off noisy logging (very useful for debugging when it's not working)
-config.set_string('-logfn', '/dev/null')
+if __name__ == '__main__':
 
-# config.set_string('-inmic', 'yes')
+    if len(sys.argv) == 2:
+        sound_file = sys.argv[1]
 
-# Decode streaming data.
-decoder = Decoder(config)
+        # Create a decoder with certain model
+        config = Decoder.default_config()
+        config.set_string('-hmm', os.path.join(base_dir,'data/cmusphinx-en-us-5.2'))
+        config.set_string('-lm', os.path.join(base_dir, 'data/en-70k-0.2.lm.bin'))
+        config.set_string('-dict', os.path.join(get_model_path(), 'cmudict-en-us.dict'))
+        # switch off noisy logging (very useful for debugging when it's not working)
+        config.set_string('-logfn', '/dev/null')
 
-# input file must be 16KHz mono for this to work
-decoder.start_utt()
-stream = open(os.path.join(base_dir, 'data/r_nixon_short.wav'), 'rb')
-while True:
-    buf = stream.read(1024)
-    if buf:
-        decoder.process_raw(buf, False, False)
+        # Decode streaming data.
+        decoder = Decoder(config)
+
+        # input file must be 16KHz mono for this to work
+        decoder.start_utt()
+        stream = open(os.path.join(base_dir, sound_file), 'rb')
+        while True:
+            buf = stream.read(1024)
+            if buf:
+                decoder.process_raw(buf, False, False)
+            else:
+                break
+        decoder.end_utt()
+
+        # output decoded text for the entire file
+        print(decoder.hyp().hypstr)
+
+        # output segments with start frames (default frame rate = 100 fps) for each
+        segment_list = []
+        first_frame = -1
+        for seg in decoder.seg():
+            if first_frame == -1:
+                first_frame = seg.start_frame
+            orig_num = (seg.start_frame - first_frame)
+            num = orig_num / 100
+            mins = int(num / 60)
+            secs = int(num % 60)
+            time_str = '%s.%s:%s' % (mins, str(secs).zfill(2), str(orig_num % 100).zfill(2))
+            segment_list.append(seg.word + "/" + time_str)
+        print(segment_list)
+
     else:
-        break
-decoder.end_utt()
-
-hypothesis = decoder.hyp()
-# logmath = decoder.get_logmath()
-print('Best hypothesis: ' + hypothesis.hypstr) # , " model score: ", hypothesis.best_score, " confidence: ", logmath.exp(hypothesis.prob))
-
-# output segments
-for seg in decoder.seg():
-    print(seg.word + " [" + str(seg.start_frame) + "-" + str(seg.end_frame) + "]")
+        print("takes one parameter: /path/to/16KHz mono wav file.wav")
